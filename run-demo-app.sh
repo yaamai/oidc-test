e61c60941f346a4511f39f141c92749237e3bcae
aina <>
2018-11-10 18:57:18 +0900

    Work with ssl

---
+++ run-demo-app.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+docker-compose exec hydra hydra --endpoint http://hydra:4445 \
+  clients delete \
+    test-client
+
+docker-compose exec hydra hydra --endpoint http://hydra:4445 \
+  clients create \
+    --id test-client \
+    --secret test-secret \
+    --response-types code,id_token \
+    --grant-types refresh_token,authorization_code,implicit \
+    --scope openid,offline,email,profile \
+    --callbacks https://10.168.142.1:4446/callback,https://10.168.142.1:5000/v3/OS-FEDERATION/identity_providers/myidp/protocols/openid/auth
+
+docker-compose exec hydra hydra --endpoint http://hydra:4445 \
+  clients delete \
+    openstack
+
+docker-compose exec hydra hydra --endpoint http://hydra:4445 \
+  clients create \
+    --id openstack \
+    --secret openstackopenstack \
+    --response-types code,id_token \
+    --grant-types refresh_token,authorization_code \
+    --scope openid,offline \
+    --callbacks https://10.168.142.1:4446/callback
+
+docker-compose exec hydra hydra --skip-tls-verify --endpoint http://hydra:4445 \
+  token user \
+    --scope openid,offline \
+    --client-id test-client \
+    --client-secret test-secret \
+    --auth-url https://10.168.142.1:4444/oauth2/auth \
+    --token-url https://10.168.142.1:4444/oauth2/token \
+    --redirect https://10.168.142.1:4446/callback
